d5a4a58e7c0e2f5fdf926d3ad8ac8af0effffff3
CtrlAltDelet <38282439+CtrlAltDelet@users.noreply.github.com>
2024-03-24 18:54:42 +0800

    change push content

--- test.py
+++ test.py
@@ -2,8 +2,8 @@
 import requests
 import sys
 token = sys.argv[1] #在pushplus网站中可以找到
-title= '标题testt' #改成你要的标题内容
-content ='内容testt' #改成你要的正文内容
+title= 'git action test' #改成你要的标题内容
+content ='git action content testt' #改成你要的正文内容
 url = 'http://www.pushplus.plus/send?token='+token+'&title='+title+'&content='+content
 requests.get(url)
 
