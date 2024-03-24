1e0c5f9e6952ea7ab2f9fd1fa69da1994ac4af03
CtrlAltDelet <38282439+CtrlAltDelet@users.noreply.github.com>
2024-03-24 18:37:55 +0800

    fix argv par error

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # encoding:utf-8
 import requests
 import sys
-token = sys.arg[1] #在pushplus网站中可以找到
+token = sys.argv[1] #在pushplus网站中可以找到
 title= '标题testt' #改成你要的标题内容
 content ='内容testt' #改成你要的正文内容
 url = 'http://www.pushplus.plus/send?token='+token+'&title='+title+'&content='+content
